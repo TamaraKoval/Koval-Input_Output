ad8d0e551b20d93ba4c41bfe86de43a0ee4d11b5
TamaraKoval <kovalpsy@mail.ru>
2023-02-11 20:30:35 +0300

    Basket v1

--- src/Basket.java
+++ src/Basket.java
@@ -1,2 +1,69 @@
+import java.util.Arrays;
+
 public class Basket {
+    private String[] products;
+    private int[] prices;
+    private int[] purchases;
+
+    public Basket(String[] products, int[] prices) throws Exception{
+        this.products = products;
+        this.prices = prices;
+        if (products.length != prices.length) {
+            throw new RuntimeException("Длина списка товаров не соответствует длине списка цен");
+        }
+        purchases = new int[products.length];
+    }
+
+    public boolean addToCart(int productNum, int amount) {
+        if (productNum >= products.length) {
+            return false;
+        } else {
+            purchases[productNum] += amount;
+            return true;
+        }
+    }
+
+    public void printProducts() {
+        System.out.println("Список доступных для покупки товаров:");
+        for (int i = 0; i < products.length; i++) {
+            System.out.println((i+1) + ". " + products[i] + " " + prices[i] + " руб/шт");
+        }
+    }
+
+    public void printCart() {
+        System.out.println("В вашей корзине:");
+        int k = 0;
+        for (int i = 0; i < products.length; i++) {
+            if (purchases[i] > 0) {
+                int productCost = purchases[i] * prices[i];
+                System.out.println(products[i] + " " + purchases[i] + " шт - " + productCost + " руб");
+                k++;
+            }
+        }
+        if (k == 0) {
+            System.out.println("Пока ничего нет");
+        }
+    }
+
+    public int sumTotal() {
+        int sumTotal = 0;
+        for (int i = 0; i < products.length; i++) {
+            if (purchases[i] > 0) {
+                int productCost = purchases[i] * prices[i];
+                sumTotal += productCost;
+                }
+            }
+            return sumTotal;
+        }
+
+    public String[] getProducts() {
+        return products;
+    }
+
+    @Override
+    public String toString() {
+        return "Basket{" +
+                "purchases=" + Arrays.toString(purchases) +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,74 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Scanner scanner = new Scanner(System.in);
 
-        String[] products = {"Молоко", "Сок", "Сыр", "Творог", "Хлеб"};
-        int[] prices = {60, 80, 300, 150, 50};
-        int[] purchases = new int[products.length];
+        Basket basket = new Basket(new String[]{"Молоко", "Сок", "Сыр", "Творог", "Хлеб"},
+                new int[]{60, 80, 300, 150, 50});
 
-        System.out.println("Список доступных для покупки товаров:");
-        for (int i = 0; i < products.length; i++) {
-            System.out.println((i+1) + ". " + products[i] + " " + prices[i] + " руб/шт");
-        }
+        basket.printProducts();
         System.out.println("Входим в режим покупок!");
 
         while (true) {
             System.out.println("Введите через пробел номер продукта и его количество или введите 'end'");
             String input = scanner.nextLine();
 
-            if ("end".equals(input)) {
-                System.out.println("Процесс покупок завершен. Идет подсчет корзины...................");
-                break;
-            }
+            if (ending(input)) break;
 
             String[] productAndPrice = input.split(" ");
-            int productNumber = Integer.parseInt(productAndPrice[0]) - 1;
-            int productCount = Integer.parseInt(productAndPrice[1]);
-            purchases[productNumber] += productCount;
-        }
+            if (inputError(productAndPrice)) continue;
 
-        System.out.println("Ваша корзина:");
-        int sumTotal = 0;
-        for (int i = 0; i < products.length; i++) {
-            if (purchases[i] > 0) {
-                int productCost = purchases[i] * prices[i];
-                System.out.println(products[i] + " " + purchases[i] + " шт - " + productCost + " руб");
-                sumTotal += productCost;
+            try {
+                int productNum = Integer.parseInt(productAndPrice[0]) -1;
+                if (productNumError(basket, productNum)) continue;
+
+                int amount = Integer.parseInt(productAndPrice[1]);
+                if (amountError(amount)) continue;
+
+                if (basket.addToCart(productNum, amount)) {
+                    System.out.println("Товар успешно добавлен");
+                }
+
+            } catch (NumberFormatException exception) {
+                System.out.println("Вы вводите не цифры, а что-то другое! Повторите попытку");
             }
         }
-        System.out.println("Итоговая сумма: " + sumTotal + " руб");
+
+        basket.printCart();
+        System.out.println("Итоговая сумма: " + basket.sumTotal() + " руб");
+        System.out.println(basket); // для проверки
+    }
+
+    private static boolean ending(String str) {
+        if ("end".equals(str)) {
+            System.out.println("Процесс покупок завершен. Идет подсчет корзины...................");
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean inputError(String[] input) {
+        if (input.length != 2) {
+            System.out.println("Данные введены не корректно, повторите попытку");
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean productNumError(Basket basket, int productNum) {
+        if (productNum >= basket.getProducts().length || productNum < 0) {
+            System.out.println("Нет продукта под номером " + (productNum + 1) + "!");
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean amountError(int amount) {
+        if (amount < 0) {
+            System.out.println("Нельзя купить отрицательное число товаров. Или вы хотите нам его подарить?=)))");
+            return true;
+        }
+        return false;
     }
 }
